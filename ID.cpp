f39b2478873972cf820fd5835b15397dc0044297
lure7991 <lure7991@colorado.edu>
2017-03-08 21:08:22 -0500

    Started adding stuff to convert hex value to binary

--- ID.cpp
+++ ID.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void printBinary(int input){
+  int tooLong = 0;
+  while(input || (tooLong<=52)){
+    if(input) printf("%d",1);
+    else printf("%d",0);
+    input >>= 1;
+    tooLong++;
+  }
+  printf("\n");
+}
+
 int main(){
-  
-  return 0;
+  int input = 0x014B4820;
+  int test = 0b10101010101010101010101010101010;
+  printf("%x\n",input);
+  printBinary(test);
+  return(0);
 }
