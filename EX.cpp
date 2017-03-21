51e741d1169db39f7913e41c323226049e0815f5
lure7991 <lure7991@colorado.edu>
2017-03-21 19:56:23 -0400

    changed input variable format, cleaned code up a bit, added input and ouput print statments so user can see what changed

--- EX.cpp
+++ EX.cpp
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
-int reg[32];
+//fill register with numbers to be used in testing, normally not needed
+int reg[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
 
+//initalize variables
 int opcode;
 int function;
-int rd;
-int rt;
-int rs;
 int shamt;
 int immediate;
 int address;
+int pc;	
+int rd;
+int rt;
+int rs;
 
-void executeR(){
-	switch(function){
+void executeR(){ //function to check for specific r-type command and execute
+	printf("\nStarting Values:\n");
+	printf("PC = %X\n",pc);
+	printf("Function = %X\n",function);
+  printf("Shamt = %d\n",shamt);
+	printf("Rd = %d\n",rd);
+  printf("Rt = %d\n",rt);
+  printf("Rs = %d\n\n",rs);
+
+	switch(function){ //choose command based on r-type function
 		case 0x20: printf("Executing: add\n");
 						rd = rs + rt;
 						printf("rd = %d\n",rd);
@@ -24,7 +35,8 @@ void executeR(){
 						printf("rd = %d\n",rd);
             break;
 		case 0x08: printf("Executing: jr\n");
-            break;
+            pc = rs;
+						break;
 		case 0x27: printf("Executing: nor\n");
             rd = ~(rs | rt);
 						printf("rd = %d\n",rd);
@@ -55,6 +67,13 @@ void executeR(){
 		case 0x23: printf("Executing: subu\n");
             break;
   }   
+	printf("\nEnding Values:\n");
+	printf("PC = %X\n",pc);
+	printf("Function = %X\n",function);
+  printf("Shamt = %d\n",shamt);
+	printf("Rd = %d\n",rd);
+  printf("Rt = %d\n",rt);
+  printf("Rs = %d\n\n",rs);
 }
 
 void executeI(){
@@ -66,34 +85,36 @@ void executeJ(){
 }
 
 int main(){
-
-	//fill the registers with their number for easy testing
-	//normally the reg values are filled in from ID
-	int i;
-	for(i=0;i<32;i++)reg[i]=i;
+	//put exmaple values in here:  ///////////////////////////////
+	//normally these would be passed in from previous pipelines///
 	
 	//example for R-Type: ADD $t1 $t2 $t3
 	opcode = 0;
 	function = 0x20;
-	rd = reg[9];
-	rt = reg[11];
-	rs = reg[10];
 	shamt = 0;
-	immediate = 0;
-	address = 0;
+	//immediate;
+	//address;
+	//pc;	
+	int rd_reg = 9;
+	int rt_reg = 10;
+	int rs_reg = 11;
 	
-	//Other examples that can be used to test:
-	  //R-Type: ADD $t1 $t2 $t3
-    //I-Type: addi t0 t1 0x0007
-    //I-Type: andi $t0,$zero,0xFFFF
-    //J-Type: j 0x0004
+	//////////////////////////////////////////////////////////////
 	
-  switch(opcode){
+	//start by taking in reg values
+	rd = reg[rd_reg]; 
+	rt = reg[rt_reg];
+	rs = reg[rs_reg];
+	
+  switch(opcode){ //select format
     case 0: executeR();
+						reg[rd_reg] = rd; //save the new rd back to the reg
+						//if command is jr, note that pc is changed in the executeR() 
             break;
     case 0x2: executeJ();
             break;
     default: executeI();
   }
+	
   return(0);
 }
