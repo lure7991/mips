26e5c6aa1f61ad428f846c33270bbff039cff15e
Alicia Harper <aliciaharper@rgnt1-60-159-dhcp.int.colorado.edu>
2017-03-22 17:37:04 -0600

    IF read PC and Inst from file. Gives HEX value of PC and Inst. Sets mem[PC]= hexInstruction

--- IF.hpp
+++ IF.hpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 
 //Fetch an instruction from instruction memory 
@@ -11,15 +14,51 @@
 //Get PC val from main which may come from outside file?
 class Fetch{
 	 public:
-	 	int instruction;
-	 	int PC;
+	 	unsigned long hexInstruction;
+	 	unsigned long hexPC;
+	 	string instruction;
+	 	string PC; //Change to array to hold multiple instructions? 
 	 	Fetch (); 
-	 	void fetchIF();
+	 	void getPC();
+	 	void getInst();
+	 	void fetchIF(int global_mem[]);
 
 } IF; 
 
 Fetch::Fetch() {
 	// open a file in read mode.
-   	int PC; 
-   	
+	instruction = '0';
+	PC= '0';
+}
+
+void Fetch::getPC(){ 
+	ifstream pcFile; 
+	pcFile.open("PC.txt");
+	if (pcFile.is_open()){
+		while(getline (pcFile, PC)){
+			hexPC = stol(PC,nullptr, 16);
+			// cout<<hexPC<< endl;
+			// cout<<PC<<endl;
+		}
+	}
+	pcFile.close();
+}
+
+void Fetch::getInst(){
+	ifstream instructionFile;
+	instructionFile.open("instruction.txt");
+	if(instructionFile.is_open()){
+		while (getline(instructionFile, instruction)){
+			hexInstruction= stol(instruction, nullptr, 16);
+
+			cout<<hexInstruction<<endl;
+			cout<<instruction<<endl;
+			//Place values into array ALT
+		}
+	}
+}
+
+void Fetch::fetchIF(int global_mem[]){
+	global_mem[hexPC]= hexInstruction;
+	cout<<global_mem[hexPC]<<endl; 
 }
