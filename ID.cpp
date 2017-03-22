d463909553213a8a8d6a3451bd1c2ca48e99e669
lure7991 <lure7991@colorado.edu>
2017-03-22 18:11:16 -0400

    Fixed decoding for immmediate values that are negative Please enter the commit message for your changes. Lines starting

--- ID.cpp
+++ ID.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 class Decode{
     // int opcode,rs,rt,rd, shamt, funct, immediate,address;
     public:
-        int opcode,rs,rt,rd, shamt, funct, immediate,address, input;
+        int opcode,rs,rt,rd, shamt, immediate, funct,address, input;
         Decode ();
         void decodeR();
         void decodeI();
@@ -13,10 +14,11 @@ class Decode{
 Decode::Decode(){
     // Add input as a parameter for later rev
 
-    int input = 0x014B4820; //R-Type: ADD $t1 $t2 $t3
+    //int input = 0x014B4820; //R-Type: ADD $t1 $t2 $t3
     //int input = 0x21280007; //I-Type: addi t0 t1 0x0007
     //int input = 0x3008FFFF; //I-Type: andi $t0,$zero,0xFFFF
-    // input = 0x08000004; //J-Type: j 0x0004
+    input = 0x2149FFAF; //I-Type: andi t1 t2 0xFFFC with 0xfffc = -4 decimal
+    //int input = 0x08000004; //J-Type: j 0x0004
     
     opcode = input >> 26;
 
@@ -39,10 +41,16 @@ void Decode::decodeR(){
 }
 
 void Decode::decodeI(){
+  //input = 0x21497FFF;
   printf("I-Type\n");
   rs = ((input >> 21) & 0b00000000000000000000000000011111);
   rt = ((input >> 16) & 0b00000000000000000000000000011111);
   immediate = (input & 0b00000000000000001111111111111111);
+  if(immediate & 0b00000000000000001000000000000000){
+    immediate = immediate & 0b00000000000000000111111111111111;
+    immediate = immediate ^ 0b00000000000000000111111111111111;
+    immediate = -1 * (immediate + 0b00000000000000000000000000000001);
+  } 
   printf("Rs = %d\n",rs);
   printf("Rt = %d\n",rt);
   printf("Immediate = %d\n",immediate);
